312602b9f1581ef851305f54d8cbfe0b29f984fc
Sindhu Pillai <sindhupillai@Sindhus-MacBook-Pro.local>
2021-08-01 14:39:36 -0700

    changes made to the files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = require('./routes');
+// Import sequelize object, 
+// Database connection pool managed by Sequelize.
+const sequelize = require('./config/connection')
+// import sequelize connection
+// Include Sequelize module.
+// const Sequelize = require('sequelize')
+
+const app = express();
+const PORT = process.env.PORT || 3006;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// Sync all models that are not
+// already in the database
+sequelize.sync({force: false}).then(() => {
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => 
+  console.log(`App listening on port ${PORT}!`));
+});
